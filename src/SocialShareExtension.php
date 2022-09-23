a07925e38faba4e6aea9a2d8fd10b63f38716cd4
Thomas Portelange <thomas@lekoala.be>
2022-09-23 09:53:07 +0200

    more bulletproof link logic

--- src/SocialShareExtension.php
+++ src/SocialShareExtension.php
@@ -2,6 +2,8 @@
 
 namespace LeKoala\CommonExtensions;
 
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\Director;
 use SilverStripe\ORM\DataExtension;
 
 /**
@@ -17,27 +19,39 @@ use SilverStripe\ORM\DataExtension;
  */
 class SocialShareExtension extends DataExtension
 {
+    protected function getSocialAbsoluteLink($obj)
+    {
+        if ($obj->hasMethod('AbsoluteLink')) {
+            $link = $obj->AbsoluteLink();
+        } elseif ($obj->hasMethod('Link')) {
+            $link = Director::absoluteURL($obj->Link());
+        } else {
+            $link = Controller::curr()->getRequest()->getURL();
+        }
+        return $link;
+    }
+
     public function FacebookShareUrl()
     {
-        $link = $this->owner->AbsoluteLink();
+        $link = $this->getSocialAbsoluteLink($this->owner);
         return 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode($link);
     }
 
     public function TwitterShareUrl()
     {
-        $link = $this->owner->AbsoluteLink();
+        $link = $this->getSocialAbsoluteLink($this->owner);
         return 'http://twitter.com/share?url=' . urlencode($link) . '&text=' . urlencode($this->owner->Title);
     }
 
     public function LinkedInShareUrl()
     {
-        $link = $this->owner->AbsoluteLink();
+        $link = $this->getSocialAbsoluteLink($this->owner);
         return 'https://www.linkedin.com/shareArticle?mini=true&url=' . urlencode($link);
     }
 
     public function EmailShareLink()
     {
-        $link = $this->owner->AbsoluteLink();
+        $link = $this->getSocialAbsoluteLink($this->owner);
         $body = _t('SocialExtension.DISCOVER', 'I discovered ') . ' "' . $this->owner->Title . '" \n' .
             _t('SocialExtension.SEE', 'You can see it here :') . ' ' . $link;
         return 'mailto:?subject=' . $this->owner->Title . '&body=' . htmlentities($body);
