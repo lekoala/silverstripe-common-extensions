0396db05451c582432e49f3d5e75147a26ed2f6e
Thomas <thomas@lekoala.be>
2023-06-01 13:24:24 +0200

    make field read only

--- src/EditorTrackingExtension.php
+++ src/EditorTrackingExtension.php
@@ -2,6 +2,7 @@
 
 namespace LeKoala\CommonExtensions;
 
+use SilverStripe\Forms\FieldList;
 use SilverStripe\Security\Member;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Security\Security;
@@ -9,7 +10,7 @@ use SilverStripe\Security\Security;
 /**
  * Track who edited what
  *
- * @property \LeKoala\CommonExtensions\EditorTrackingExtension $owner
+ * @property \LeKoala\CommonExtensions\EditorTrackingExtension|\SilverStripe\ORM\DataObject $owner
  * @property int $CreatedByID
  * @property int $LastEditedByID
  * @method \SilverStripe\Security\Member CreatedBy()
@@ -29,5 +30,17 @@ class EditorTrackingExtension extends DataExtension
             $this->owner->CreatedBy = $user->ID ?? 0;
         }
         $this->owner->LastEditedBy = $user->ID ?? 0;
+    }
+
+    public function updateCMSFields(FieldList $fields)
+    {
+        $CreatedByID = $fields->dataFieldByName('CreatedByID');
+        if ($CreatedByID) {
+            $fields->replaceField('CreatedByID', $CreatedByID->performReadonlyTransformation());
+        }
+        $LastEditedByID = $fields->dataFieldByName('LastEditedByID');
+        if ($LastEditedByID) {
+            $fields->replaceField('LastEditedByID', $LastEditedByID->performReadonlyTransformation());
+        }
     }
 }
